2eecc76d5c6b8361cec2a4da8aa18f69d3319978
Samuel Panicucci <samuel.panicucci@gmail.com>
2026-04-29 08:38:42 +0200

    Modificata la struttura del progetto secondo il file di README

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // Bootstrap JS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import './index.css'
+import './styles/index.css'
 
 
 import App from './App.jsx'
